0788ad9a7fcaff4815355577b14956be9b16c47c
Bartosz Grzybowski <bartoszgrzybo2001@gmail.com>
2024-05-06 13:42:44 +0200

    Dodaj pliki projektów.

---
+++ BlazorWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebApp.Components.Classes;
+
+namespace BlazorWebApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<BlazorWebApp.Components.Classes.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ BlazorWebApp/Data/Migrations/20240506102136_added_class_movie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_class_movie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
